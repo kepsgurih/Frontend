8ebda49dcc3bfc6a52e07eb3177927a1e86d5239
Kevin <kevin@Kevins-MacBook-Pro.local>
2023-01-28 17:34:17 +0700

    first commit

---
+++ pages/About.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function About(){
+    return(
+        <>
+        <Head>
+            <title>About Us</title>
+        </Head>
+        <div className="flex w-full">
+            <button className="mx-auto text-center bg-red-200 hover:bg-red-300 rounded-lg text-red-800 px-20">test</button>
+        </div>
+        </>
+    )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import HeaderNavbar from '../src/organism/HeaderNavbar'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+    <div className='flex flex-col h-screen"'>
+    <HeaderNavbar />
+    <main className='dark:bg-slate-800 dark:text-gray-200 text-slate-700  h-screen pt-16 md:pt-20'>
+   <Component {...pageProps} />
+    </main>
+    </div>
+  )
 }
